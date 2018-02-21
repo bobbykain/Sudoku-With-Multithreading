ed108df04f75bc8bede82790398b66bf4fc8a189
Bobby Kain <robertmkain@gmail.com>
2018-02-21 13:17:40 -0800

    got rid of csv reader file, combined countNums w/ parser

--- Sudoku.java
+++ Sudoku.java
@@ -1,25 +1,68 @@
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 
-
-public class Sudoku implements Runnable{
-    int[][] myPuzzle;
-    int[][] rowNums;
-    int[][] colNums;
-    int[][] boxNums;
+public class Sudoku implements Runnable {
+    int[][] myPuzzle;   // 9x9 array of sudoku board
+    int[][] rowNums;    // 9x9 array of number of times a number appears in each row
+    int[][] colNums;    // 9x9 array of number of times a number appears in each col
+    int[][] boxNums;    // 9x9 array of number of times a number appears in each box
 
     public Sudoku(String fileName) {
-        myPuzzle = CSVReader.readPuzzle(fileName);
+        //myPuzzle = CSVReader.readPuzzle(fileName);
+        myPuzzle = new int[9][9];
         rowNums = new int[9][10];
         colNums = new int[9][10];
         boxNums = new int[9][10];
+        initialize(fileName);
+    }
+
+    public void initialize(String fileName) {
+        try {
+            BufferedReader br = new BufferedReader(new FileReader(fileName));
+            String line = "";
+            for (int row = 0; row < 9; ++row) {
+                line = br.readLine();
+                String[] row_arr = line.split(",");
+                for (int col = 0; col < 9; ++col) {
+                    int num = Integer.parseInt(row_arr[col]);
+                    ++rowNums[row][num];
+                    ++colNums[col][num];
+                    if (row < 3 && col < 3)
+                        ++boxNums[0][num];
+                    else if (row < 3 && (col > 2 && col < 6))
+                        ++boxNums[1][num];
+                    else if (row < 3 && col > 5)
+                        ++boxNums[2][num];
+                    else if ((row > 2 && row < 6) && col < 3)
+                        ++boxNums[3][num];
+                    else if ((row > 2 && row < 6) && (col > 2 && col < 6))
+                        ++boxNums[4][num];
+                    else if ((row > 2 && row < 6) && col > 5)
+                        ++boxNums[5][num];
+                    else if (row > 5 && col < 3)
+                        ++boxNums[6][num];
+                    else if (row > 5 && (col > 2 && col < 6))
+                        ++boxNums[7][num];
+                    else
+                        ++boxNums[8][num];
+                }
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     public void run() {
 
     }
 
-    public void countNums() {
+    /*public void countNums() {
         for (int row = 0; row < 9; ++row) {
-            for (int col = 0; col < 9; ++ col) {
+            for (int col = 0; col < 9; ++col) {
                 int num = myPuzzle[row][col];
                 ++rowNums[row][num];
                 ++colNums[col][num];
@@ -43,11 +86,10 @@ public class Sudoku implements Runnable{
                     ++boxNums[8][num];
             }
         }
-    }
+    }*/
 
     public static void main(String[] args) {
-        Sudoku puzzle = new Sudoku("csv/ex1.csv");
-        puzzle.countNums();
+        Sudoku puzzle = new Sudoku("csv/ex2.csv");
 
         for (int i = 0; i < 9; ++i) {
             for (int j = 0; j < 9; ++j) {
